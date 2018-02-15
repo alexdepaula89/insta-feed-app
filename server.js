ad551567304c3626403dce5163999e9fd2308840
Perola Da Cruz <perola.dacruz@talktalkplc.com>
2018-02-15 18:33:14 +0000

    First Commit

---
+++ server.js
@@ -0,0 +1,33 @@
+// GRAB THE PACKAGES/VARIABLES WE NEED
+// ==================================================
+var express = require('express');
+var app = express();
+var ig = require('instagram-node').instagram();
+// CONFIGURE THE APP
+// ==================================================
+
+// configure instagram app with your access_token
+ig.use({
+access_token: '...',
+});
+
+// tell node where to look for site resources
+app.use(express.static(__dirname + '/public'));
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+// configure instagram app with your access token
+// we'll get to this soon
+// SET THE ROUTES
+// ===================================================
+// home page route - our profile's images
+app.get('/', function(req, res) {
+// use the instagram package to get our profile's media
+ig.user_self_media_recent(function(err, medias, pagination, remaining, limit) {
+// render the home page and pass in our profile's images
+res.render('pages/index', { grams: medias });
+});
+});
+// START THE SERVER
+// ==================================================
+app.listen(8080);
+console.log('App started! Look at http://localhost:8080');
